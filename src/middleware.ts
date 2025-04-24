8168784eb3c50b229d29e5b80f3b0437927068de
reynardaj <reynard.joshua4949@gmail.com>
2025-04-24 20:27:42 +0700

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,11 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    // Match all non-static, non-_next routes for pages
+    '/((?!.*\\..*|_next).*)',
+    // API routes
+    '/api/(.*)',
+    // tRPC routes
+    '/trpc/(.*)',
   ],
 };
